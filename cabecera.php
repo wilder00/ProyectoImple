9af7b22108500ebf9aa81beb736757ed4087f3c2
Wilder Trujillo <nadie437@gmail.com>
2018-11-18 15:14:34 -0500

    cabecera de juan valdez

--- cabecera.php
+++ cabecera.php
@@ -1,38 +1,22 @@
-<div id="titulo">
-    <img src="imagenes\fondo.jpg" alt="">
-    
-
-<div id="cabecera">
-    <nav>           
-    <div>
-            
-        <div id="comandar">
-            <button id="mostrara"><img src="imagenes\gi.png" alt="" id="imagen"></button>
-        </div>
-
-        <div id="registro" style="display:none">
-            <ul>
-                <li><a href="intranet/login.html">Intranet</a></li>
-            </ul>
-        </div>
-        
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-        <script>
-            $("#mostrara").click(function(){
-                var estado = $("#registro").css("display");
-                if (estado == "none") {
-                    $("#registro").show();
-                    $("#mostrara").img("imagenes\gi.png");
-                } else {
-                    $("#registro").hide();
-                    $("#mostrara").img("imagenes\gi.png");
-                }
-               
-            });
-        </script>
-    
-    </div>
-    
-    </nav>
-</div>
-</div>
+<nav class="navbar navbar-expand-lg juan-valdez-background juan-valdez-text-color ">
+  <a class="navbar-brand" href="#"><img src="http://www.juanvaldezcafe.com/sites/all/themes/bootstrap/jv_col/logo.png" alt="Juan Valdez"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon ">↕</span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto juan-valdez-list-group">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Navbar text with an inline element
+    </span>
+  </div>
+</nav>
